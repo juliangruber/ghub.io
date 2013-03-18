b806e3e3967cf355c1d0a1d2137ad80e829c570c
Julian Gruber <julian@juliangruber.com>
2013-03-18 14:41:16 +0100

    use webtap for tests

--- test/test.js
+++ test/test.js
@@ -1,107 +1,59 @@
-var http = require('http');
-var tap = require('tap');
+var webtap = require('webtap');
 var ghub = require('..');
 var fs = require('fs');
 
+var test = webtap(ghub);
+
 var usage = fs.readFileSync(__dirname + '/../static/index.html').toString();
 
-test('GET /<valid-package>', function (t) {
-  t.req('GET', '/review', function (res) {
-    t.equal(res.headers.location, 'https://github.com/juliangruber/review');
-    t.equal(res.statusCode, 302);
-    t.body('-> https://github.com/juliangruber/review');
-  });
+test('valid package', 'GET', '/review', function (t, res) {
+  t.equal(res.headers.location, 'https://github.com/juliangruber/review');
+  t.equal(res.statusCode, 302);
+  t.body('-> https://github.com/juliangruber/review');
 });
 
-test('HEAD /<valid-package>', function (t) {
-  t.req('HEAD', '/review', function (res) {
-    t.equal(res.headers.location, 'https://github.com/juliangruber/review');
-    t.equal(res.statusCode, 302);
-    t.body('');
-  });
+test('valid package', 'HEAD', '/review', function (t, res) {
+  t.equal(res.headers.location, 'https://github.com/juliangruber/review');
+  t.equal(res.statusCode, 302);
+  t.body('');
 });
 
-test('GET /<package-not-found>', function (t) {
-  t.req('GET', '/sdf098sdf098', function (res) {
-    t.equal(res.headers.location, 'http://npmjs.org/sdf098sdf098');
-    t.equal(res.statusCode, 302);
-    t.body('-> http://npmjs.org/sdf098sdf098');
-  });
+test('not found', 'GET', '/sdf098sdf098', function (t, res) {
+  t.equal(res.headers.location, 'http://npmjs.org/sdf098sdf098');
+  t.equal(res.statusCode, 302);
+  t.body('-> http://npmjs.org/sdf098sdf098');
 });
 
-test('GET /<package-without-repository>', function (t) {
-  t.req('GET', '/mysql', function (res) {
-    t.equal(res.headers.location, 'http://npmjs.org/mysql');
-    t.equal(res.statusCode, 302);
-    t.body('-> http://npmjs.org/mysql');
-  });
+test('without repository', 'GET', '/mysql', function (t, res) {
+  t.equal(res.headers.location, 'http://npmjs.org/mysql');
+  t.equal(res.statusCode, 302);
+  t.body('-> http://npmjs.org/mysql');
 });
 
-test('GET /<package-with-bitbucked-repo>', function (t) {
-  t.req('GET', '/program', function (res) {
-    t.equal(res.headers.location, 'http://npmjs.org/program');
-    t.equal(res.statusCode, 302);
-    t.body('-> http://npmjs.org/program');
-  });
+test('bitbucked', 'GET', '/program', function (t, res) {
+  t.equal(res.headers.location, 'http://npmjs.org/program');
+  t.equal(res.statusCode, 302);
+  t.body('-> http://npmjs.org/program');
 });
 
-test('GET /<package-with-homepage>', function (t) {
-  t.req('GET', '/ihaveahomepage', function (res) {
-    t.equal(res.headers.location, 'https://github.com/juliangruber/ghub.io');
-    t.equal(res.statusCode, 302);
-    t.body('-> https://github.com/juliangruber/ghub.io');
-  });
+test('homepage', 'GET', '/ihaveahomepage', function (t, res) {
+  t.equal(res.headers.location, 'https://github.com/juliangruber/ghub.io');
+  t.equal(res.statusCode, 302);
+  t.body('-> https://github.com/juliangruber/ghub.io');
 });
 
-test('GET /', function (t) {
-  t.req('GET', '/', function (res) {
-    t.equal(res.headers['content-type'], 'text/html; charset=UTF-8');
-    t.equal(res.statusCode, 200);
-    t.body(usage);
-  });
+test('GET' ,'/', function (t, res) {
+  t.equal(res.headers['content-type'], 'text/html; charset=UTF-8');
+  t.equal(res.statusCode, 200);
+  t.body(usage);
 });
 
-test('GET /robots.txt', function (t) {
-  t.req('GET', '/robots.txt', function (res) {
-    t.equal(res.statusCode, 404);
-    t.body('File not found. :(');
-  })
+test('GET', '/robots.txt', function (t, res) {
+  t.equal(res.statusCode, 404);
+  t.body('File not found. :(');
 });
 
-test('GET /favicon.ico', function (t) {
-  t.req('GET', '/favicon.ico', function (res) {
-    t.equal(res.statusCode, 404);
-    t.body('File not found. :(');
-  })
+test('GET', '/favicon.ico', function (t, res) {
+  t.equal(res.statusCode, 404);
+  t.body('File not found. :(');
 });
-
-function test (name, cb) {
-  tap.test(name, function (t) {
-    t.req = req;
-    ghub.listen(cb.bind(null, t));
-
-    function req (method, path, cb) {
-      var opts = {
-        hostname : 'localhost',
-        port : ghub.address().port,
-        path : path,
-        method : method
-      }
-      var req = http.request(opts, function (res) {
-        t.body = body;
-        cb(res);
-
-        function body (str) {
-          var data = '';
-          res.on('data', function (d) { data += d });
-          res.on('end', function () {
-            t.equal(data, str);
-            ghub.close(t.end.bind(t));
-          });
-        }
-      })
-      req.on('error', t.notOk.bind(t));
-      req.end();
-    }
-  });
-}
